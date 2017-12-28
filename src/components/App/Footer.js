1adc490b894985ac0f0530d30be8d765a86c03d7
J. Graves <jwgravesfl@live.com>
2017-12-28 14:55:44 -0500

    Facebook Login Working

--- src/components/App/Footer.js
+++ src/components/App/Footer.js
@@ -1,11 +1,3 @@
-/**
- * React Starter Kit for Firebase and GraphQL
- * https://github.com/kriasoft/react-firebase-starter
- * Copyright (c) 2015-present Kriasoft | MIT License
- */
-
-/* @flow */
-
 import React from 'react';
 import styled from 'styled-components';
 
@@ -15,10 +7,6 @@ const Container = styled.div`
   left: 0;
   padding: 24px;
   color: rgba(0, 0, 0, 0.4);
-`;
-
-const Copyright = styled.span`
-  padding-right: 0.5em;
 `;
 
 const Separator = styled.span`
@@ -44,8 +32,7 @@ class Footer extends React.Component<{}> {
   render() {
     return (
       <Container>
-        <Copyright css="padding-right: 0.5em">&copy; 2015-present</Copyright>
-        <ExtLink href="https://github.com/kriasoft">Kriasoft</ExtLink>
+        <ExtLink href="#">Information Design and Development</ExtLink>
         <Separator>|</Separator>
         <ExtLink href="/privacy">Privacy Policy</ExtLink>
       </Container>
